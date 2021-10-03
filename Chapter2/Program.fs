c5f43d159a94b7c98ea80282859e4839fd6f4c5d
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2021-10-03 21:13:05 +0100

    Started chap 2

---
+++ Chapter2/Program.fs
@@ -0,0 +1,15 @@
+﻿// Test harness for Cracking the Coding Interview 6th Ed. Chapter 2 solutions
+// on Linked Lists.
+
+// Proper unit tests to follow when I get round to it!
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv =
+
+    // Question 1 Remove Dups:
+    let lst = [1; 1; 1; 2; 3; 3; 4; 5; 5]
+    printfn "removeDups %A: %A" lst (question_2_1.removeDups lst)
+    0
---
+++ Chapter2/Question1.fs
@@ -0,0 +1,5 @@
+﻿module question_2_1
+
+// Remove duplicates from a linked list.
+let removeDups lst =
+    lst |> List.distinct
